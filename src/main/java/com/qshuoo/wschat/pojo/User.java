ba8ea13c4f932e6a9c891ca9415ad61e2dfd56cf
qiaoyongshuo <qiaoyongshuo@kingsoft.com>
2018-01-18 20:22:59 +0800

    add user pojo

---
+++ src/main/java/com/qshuoo/wschat/pojo/User.java
@@ -0,0 +1,68 @@
+package com.qshuoo.wschat.pojo;
+
+/**
+ * 用户
+ * @author qiaoyongshuo
+ *
+ */
+public class User {
+	
+	private Long uid; // id
+	private String uname; // 用户名
+	private String pwd; // 密码
+	private String rname; // 真实姓名
+	private String phone; // 电话
+	private String email; // 邮箱
+	private String img; // 头像
+	private String signature; // 个性签名 
+	public Long getUid() {
+		return uid;
+	}
+	public void setUid(Long uid) {
+		this.uid = uid;
+	}
+	public String getUname() {
+		return uname;
+	}
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public String getRname() {
+		return rname;
+	}
+	public void setRname(String rname) {
+		this.rname = rname;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getImg() {
+		return img;
+	}
+	public void setImg(String img) {
+		this.img = img;
+	}
+	public String getSignature() {
+		return signature;
+	}
+	public void setSignature(String signature) {
+		this.signature = signature;
+	}
+	
+	
+}
